62559b342b4e8941f33d6559ba4187a43101da76
Rajibul Khan <rajibulkhan642@gmail.com>
2022-11-11 09:46:55 +0530

    Added icons for different actions like Submit , Edit and Remove.

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let tasks=document.getElementById('tasks');
 let txt=document.getElementById('field');
 
+txt.addEventListener("keydown", function (e) {
+    if (e.key === "Enter") {  
+      validate(e);
+    }
+  });
+
+function validate(e) {
+    add();
+  }
+
 function add(e){
+    if(txt.value!=""){
     var newtask=document.createElement('div');
     newtask.innerHTML=`<h3 style="display:inline;font-family: 'Edu QLD Beginner', cursive;
     font-family: 'Lobster', cursive;">${txt.value}</h3><button onclick="edit(this)" style="border:none;border-radius:10px;"><i class="bi bi-pencil-square"></i></button><button onclick="dlt(this)" style="border:none;border-radius:10px;"><i class="bi bi-x-circle-fill"></i></button>`
@@ -9,7 +20,7 @@ function add(e){
     newtask.style="display:grid;grid-gap:5px;grid-template-columns:5fr 1fr 1fr;padding:2em;margin:2em;background:pink;border:none;border-radius:40px"
    
     tasks.appendChild(newtask);
-    txt.value="";
+    txt.value="";}
 }
 function dlt(e){
     e.parentElement.remove();
@@ -17,5 +28,7 @@ function dlt(e){
 function edit(e){
     var entry;
     entry=prompt("Enter to Edit");
-    e.parentElement.innerHTML=`<h3 style="display:inline">${entry}</h3><button onclick="edit(this)" style="border:none;border-radius:10px;"><i class="bi bi-pencil-square"></i></button><button onclick="dlt(this)" style="border:none;border-radius:10px;"><i class="bi bi-x-circle-fill"></i></button>`;
+    if(entry!=null){
+    e.parentElement.innerHTML=`<h3 style="display:inline;font-family: 'Edu QLD Beginner', cursive;
+    font-family: 'Lobster', cursive;">${entry}</h3><button onclick="edit(this)" style="border:none;border-radius:10px;"><i class="bi bi-pencil-square"></i></button><button onclick="dlt(this)" style="border:none;border-radius:10px;"><i class="bi bi-x-circle-fill"></i></button>`;}
 }
